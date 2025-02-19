5850eba02d8d2c37f80ab8978f7eb73af37afd35
yordanossole <yordanos.sole@gmail.com>
2025-02-19 18:52:59 +0300

    update

---
+++ 268. Missing Number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        # n = len(nums) + 1
+        # for i in range(n):
+        #     if not i in nums:
+        #         return i
+        # x = set(nums)
+        # y = set(range(n))
+        # return y.difference(x).pop()
+        n = len(nums)
+        expectedSum = n * (n+1) // 2
+        actualSum = sum(nums)
+        return expectedSum - actualSum
