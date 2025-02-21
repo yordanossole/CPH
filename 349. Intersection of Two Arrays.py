07ec1984a8c8e894c621556b036cf6f050f7dee1
yordanossole <yordanos.sole@gmail.com>
2025-02-21 21:43:53 +0300

    update

---
+++ 349. Intersection of Two Arrays.py
@@ -0,0 +1,23 @@
+class Solution:
+    def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # result = []
+        # if len(nums1) > len(nums2):
+        #     longer = nums1
+        #     shorter = nums2
+        # else:
+        #     longer = nums2
+        #     shorter = nums1
+            
+        # for num in shorter:
+        #     if num in longer:
+        #         result.append(num)
+        # return list(set(result))
+
+        
+        st = set(nums1)
+        result = []
+        for num in nums2:
+            if num in mp:
+                result.append(num)
+                st.remove(num)
+        return result
